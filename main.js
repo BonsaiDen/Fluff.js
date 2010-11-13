5a9735c380079240d155b984a3e8d73dfa31816a
Ivo Wetzel <ivo.wetzel@googlemail.com>
2010-11-13 20:21:56 +0100

    Test test test.

--- main.js
+++ main.js
@@ -4,7 +4,8 @@
 // -----------------------------------------------------------------------------
 fluff.onLoad = function() {
     fluff.log('loaded!');
-    fluff.graphics.setMode(320, 240, 'Fluff Test');
+    fluff.graphics.setMode(480, 320, 'Fluff Test', 4);
+    fluff.graphics.setBackgroundColor(0, 0, 0);
 };
 
 fluff.onUpdate = function(dt) {
@@ -12,7 +13,16 @@ fluff.onUpdate = function(dt) {
 };
 
 fluff.onDraw = function() {
-    
+    fluff.graphics.clear();
+   // for(var i = 0; i < 100; i++) {
+        fluff.graphics.setColor(255, 0, 0);
+        fluff.graphics.triangle(100, 0, 50, 200, 150, 200);
+        fluff.graphics.setColor(0, 255, 0);
+        fluff.graphics.circleFilled(200, 100, 50);
+        fluff.graphics.setColor(255, 255, 0);
+        fluff.graphics.setColor(0, 255, 255);
+        fluff.graphics.rectangleFilled(10, 10, 100, 50);
+  //  }
 };
 
 fluff.onExit = function() {
